4962da7d5823a3b4f4d670ac3b536567330f18c7
Daniel <79312811+danieljbk@users.noreply.github.com>
2022-07-17 16:06:41 +0900

    don't show english lyrics in search results

--- src/routes/search.js
+++ src/routes/search.js
@@ -36,34 +36,36 @@ router.get('/multiple', async (req, res) => {
     const searchResultsOriginal = await Client.songs.search(
       req.query.searchTerm
     );
-    const allSearchResults = [
-      searchResultsKorean.reverse(),
-      searchResultsOriginal.reverse(),
-    ];
+    const allSearchResults = [searchResultsKorean, searchResultsOriginal];
 
     let count = 0;
     const songs = [];
     for (let searchResult of allSearchResults) {
       for (let song of searchResult) {
         if (count < 8) {
-          const imageSrc = song._raw.song_art_image_url;
-          const geniusDefaultImageUrl =
-            'https://assets.genius.com/images/default_cover_image.png';
-          if (!imageSrc.includes(geniusDefaultImageUrl)) {
-            songs.push({
-              title: condenseWhitespace(
-                song.title
-                  .replace('(한국어 번역)', '')
-                  .trimEnd()
-                  .replace('(Korean Version)', '')
-              ),
-              artist: condenseWhitespace(
-                song.artist.name.replace('(한국어 번역)', '')
-              ),
-              imageSrc,
-              lyrics: await song.lyrics(),
-            });
-            count += 1;
+          if (
+            !song.title.toLowerCase().includes('english translation') &&
+            !song.title.toLowerCase().includes('english version')
+          ) {
+            const imageSrc = song._raw.song_art_image_url;
+            const geniusDefaultImageUrl =
+              'https://assets.genius.com/images/default_cover_image.png';
+            if (!imageSrc.includes(geniusDefaultImageUrl)) {
+              songs.push({
+                title: condenseWhitespace(
+                  song.title
+                    .replace('(한국어 번역)', '')
+                    .trimEnd()
+                    .replace('(Korean Version)', '')
+                ),
+                artist: condenseWhitespace(
+                  song.artist.name.replace('(한국어 번역)', '')
+                ),
+                imageSrc,
+                lyrics: await song.lyrics(),
+              });
+              count += 1;
+            }
           }
         }
       }
